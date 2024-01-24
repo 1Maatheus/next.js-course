1a55e3a0a75f9e76a48b87eefedfc61396c21828
Matheus Oliveira Monteiro <matheusoliveira7227@gmail.com>
2024-01-24 12:22:53 -0300

    add a loading spinner when fetchind data...

---
+++ app/loading.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./loading.module.css";
+
+const MealsLoadingPage = () => {
+  return <div className={styles.loading}></div>;
+};
+
+export default MealsLoadingPage;
