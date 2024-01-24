3913b79d98007bc42557d4730304819f01ff17e6
Matheus Oliveira Monteiro <matheusoliveira7227@gmail.com>
2024-01-24 13:24:13 -0300

    adding new pages

--- lib/meals.tsx
+++ lib/meals.tsx
@@ -3,6 +3,9 @@ const db = sql("meals.db");
 
 export async function getMeals() {
   await new Promise((resolve) => setTimeout(resolve, 2000));
-
   return db.prepare("SELECT * FROM meals").all();
+}
+
+export function getMeal({ slug }: { slug: string }) {
+  return db.prepare("SELECT * FROM meals WHERE slug = ?").get(slug);
 }
